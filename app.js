df3d09232140817d6d59aca1d571dd834a8397e1
irfan-official <irfanofficial.work@gmail.com>
2025-02-20 20:00:46 +0600

    changes in app.js

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 import express from "express";
 import { config } from "dotenv";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
+config();
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.status(200).send(hello);
+// app.use(cors()); // Allow all origin
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.get("/", (req, res, next) => {
+  try {
+    res.status(200).send(hello);
+  } catch (err) {
+    next(
+      new Error(
+        JSON.stringify({
+          previousError: err.message,
+          customError: "abe sale",
+        })
+      )
+    );
+  }
 });
 
 app.get("/user/:id", (req, res) => {
@@ -19,7 +46,7 @@ app.get("/user/:id", (req, res) => {
 
 app.use((err, req, res, next) => {
   return res.status(500).json({
-    "err.message": err.message,
+    "err.message": JSON.parse(err.message),
     "req.fresh": req.fresh,
   });
 });
